5b2e3e2e4f8b8dc7fb453bd0397c01f8230f5e44
Piotr Węgrzyn <piotreg8@gmail.com>
2022-08-14 17:16:02 +0200

    Delete backgroundMove.js

--- backgroundMove.js
+++
@@ -1,9 +0,0 @@
-export const moveEl = (e) => {
-    const box = document.querySelector(".time")
-    const mouseX = (e.clientX) - (box.getBoundingClientRect().width / 2)
-    const mouseY = e.clientY / 80
-    const title = document.querySelector("h1");
-    box.style.transform = `translate(${mouseX / 40}px,-${mouseY}px)`
-    title.style.transform = `translate(${mouseX / 80}px, -${mouseY}px)`
-}
-
