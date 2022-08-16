5929f48a15d671b410b1c0b56956d0db8ea85b86
Piotr Węgrzyn <piotreg8@gmail.com>
2022-08-16 18:40:34 +0200

    Update navigation.js

--- scripts/todo/assets/navigation.js
+++ scripts/todo/assets/navigation.js
@@ -65,6 +65,9 @@ export const removeNav = () => {
                 input.addEventListener('input', () => {
                 document.querySelector(".user__navigation").style.display = "none";
             })
+                input.addEventListener('submit', () => {
+                document.querySelector(".user__navigation").style.display = "none";
+            })
             input.addEventListener('blur', () => {
                 document.querySelector(".user__navigation").style.display = "flex";
             })
